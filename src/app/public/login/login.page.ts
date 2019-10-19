f2e2c9cf2513035c9e879d1f89b746081135986f
rafael <rafael.cmota@hotmail.com>
2019-10-19 14:43:00 -0300

    commit validacao 191020191142

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 //import { IonRadioGroup } from '@ionic/angular';
+import { StorageService } from '../../services/storage.service';
+import { ContaTemporariaService } from '../../services/conta-temporaria.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +16,7 @@ export class LoginPage implements OnInit {
 
   //@ViewChild(IonRadioGroup, {static: false}) radioGroup: IonRadioGroup
 
-  constructor(private authService: AuthService, private router: Router, private toastController: ToastController) { }
+  constructor(private contaTemporariaService: ContaTemporariaService, private storageService: StorageService, private authService: AuthService, private router: Router, private toastController: ToastController) { }
 
   ngOnInit() {
   }
@@ -29,20 +31,31 @@ export class LoginPage implements OnInit {
     else if(this.radioGroup.value === "ADM" || this.radioGroup.value === "GRT"){
       console.log("ADM || GRT")
     }*/
-    const user = await this.authService.onLogin(form.value.usuario, form.value.senha)
-    console.log(user);
-    if(user === "auth/invalid-email"){
-      return this.showToast("CONTA INVALIDA!!!");
-    }
-    else if(user === "auth/user-disabled"){
-      return this.showToast("USUARIO FOI DISABILITADO!!!");
-    }
-    else if(user === "auth/wrong-password"){
-      return this.showToast("SENHA ERRADA!!!");
-    }
-
-    this.showToast("Login Realizado com Sucesso!!!");
-    this.router.navigateByUrl('/tabs/homepage');
+    this.storageService.getUser().then(async users => {
+      if (users === null || users === undefined || !users){
+        console.log("storage não existe");
+      }
+      else{
+        console.log("storage existe " + users);
+        const user = await this.authService.onLogin(form.value.usuario, form.value.senha)
+        console.log(user);
+        if(user === "auth/invalid-email"){
+          return this.showToast("CONTA INVALIDA!!!");
+        }
+        else if(user === "auth/user-disabled"){
+          return this.showToast("USUARIO FOI DISABILITADO!!!");
+        }
+        else if(user === "auth/wrong-password"){
+          return this.showToast("SENHA ERRADA!!!");
+        }
+    
+    
+        this.showToast("Login Realizado com Sucesso!!!");
+        this.router.navigateByUrl('/tabs/homepage');
+      }
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   async showToast(msg){
